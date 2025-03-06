efc4c4a85716a3c20e2215cb18177cc2e689fe7c
aniketmanjare8995 <aniketmanjare8712@gmail.com>
2025-03-06 13:06:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) => {
+    input.addEventListener("input", (event) => {
+        if (event.inputType !== "deleteContentBackward" && input.value.length === 1) {
+            if (index < inputs.length - 1) {
+                inputs[index + 1].focus(); // Move to next input
+            }
+        }
+    });
+
+    input.addEventListener("keydown", (event) => {
+        if (event.key === "Backspace" && input.value.length === 0) {
+            if (index > 0) {
+                inputs[index - 1].focus(); // Move back to previous input
+            }
+        }
+    });
+});
